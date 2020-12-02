cbdbc6650938e39c7ce061c8174e43b79a544c92
Caíque Nunes <58406152+caiqueravpp@users.noreply.github.com>
2020-12-01 23:01:28 -0300

    changes on login, pagamentos

--- Frontend/login.php
+++ Frontend/login.php
@@ -13,11 +13,11 @@ $senha_login = mysqli_real_escape_string($connect, $_POST['senha_login']);
 
 $query = "SELECT * FROM usuarios WHERE CPF = '{$cpf_login}' and Senha = md5('{$senha_login}')";
 $result = mysqli_query($connect, $query);
-$row = mysqli_num_row($result);
+$row = mysqli_num_rows($result);
 
 if($row = 1){
-    $_SESSION['cpflogin'] = $cpf_login;
-    header('location : painel.php');
+    $_SESSION['cpf_login'] = $cpf_login;
+    header('location: cardapio.html');
     exit();
 
 
